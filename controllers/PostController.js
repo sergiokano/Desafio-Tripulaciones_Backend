ab257750798c2285ec00d09c2a26a7659aaecff2
diegopalones <diego.palones@gmail.com>
2023-01-23 11:03:06 +0100

    modified createPost endpoint for coordenades

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,24 +1,28 @@
 const Post = require("../models/Post");
 const User = require("../models/User");
 
+
 const PostController = {
-    async create(req, res, next) {
-      try {
-        if (req.file) req.body.image_path = req.file.filename;
-        const post = await Post.create({
-          ...req.body,
-          userId: req.user._id,
-        });
-        await User.findByIdAndUpdate(req.user._id, {
-          $push: { postIds: post._id },
-        });
-        res.status(201).send(post);
-        res.send({ msg: "Incidencia creada correctamente", post });
-      } catch (error) {
-        console.error(error);
-        next(error);
-      }
-    },
+  async create(req, res, next) {
+    try {
+      if (req.file) req.body.image_path = req.file.filename;
+      const longitude = req.body.longitude;
+      const latitude = req.body.latitude;
+      const post = await Post.create({
+        ...req.body,
+        userId: req.user._id,
+        longitude: longitude,
+        latitude: latitude
+      });
+      await User.findByIdAndUpdate(req.user._id, {
+        $push: { postIds: post._id },
+      });
+      res.status(201).send({ msg: "Incidencia creada correctamente", post });
+    } catch (error) {
+      console.error(error);
+      next(error);
+    }
+  },
     async getAll(req, res) {
         try {
           const { page = 1, limit = 200 } = req.query;
