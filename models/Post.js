b8131303f88de514a1fc51a22406d255194545ef
Alex Jimenez <alexjimenezada@gmail.com>
2023-01-26 21:50:53 +0100

    Post model fixed

--- models/Post.js
+++ models/Post.js
@@ -2,58 +2,58 @@ const mongoose = require("mongoose");
 const ObjectId = mongoose.SchemaTypes.ObjectId;
 
 const PostSchema = new mongoose.Schema(
-  {
-    incidence: {
-      type: String,
+    {
+        incidence: {
+            type: String,
 
-      required: [true, "Por favor, comunícanos la incidencia"],
+            required: [true, "Por favor, comunícanos la incidencia"],
+        },
+
+        description: {
+            type: String,
+
+            required: [true, "Por favor, descríbenos la incidencia"],
+        },
+
+        address: {
+            type: String,
+        },
+
+        code: {
+            type: String,
+        },
+
+        category: {
+            type: String,
+        },
+
+        subCategory: {
+            type: String,
+        },
+
+        longitude: {
+            type: Number,
+        },
+
+        latitude: {
+            type: Number,
+        },
+
+        userId: {
+            type: ObjectId,
+            ref: "User",
+        },
+        comments: [
+            {
+                userId: { type: ObjectId, ref: "User" },
+                comment: String,
+            },
+        ],
+
+        verification: [{ type: ObjectId }],
+        image_path: { type: String },
     },
-
-    description: {
-      type: String,
-
-      required: [true, "Por favor, descríbenos la incidencia"],
-    },
-
-    address: {
-      type: String,
-    },
-
-    code: {
-      type: String,
-    },
-
-    category: {
-      type: String,
-    },
-
-    subCategory: {
-      type: String,
-    },
-
-    longitude: {
-      type: Number,
-    },
-
-    latitude: {
-      type: Number,
-    },
-
-    userId: {
-      type: ObjectId,
-      ref: "User",
-    },
-    comments: [
-      {
-        userId: { type: ObjectId, ref: "Comment" },
-        comment: String,
-      },
-    ],
-
-    verification: [{ type: ObjectId }],
-    image_path: { type: String },
-  },
-  { timestamps: true }
+    { timestamps: true }
 );
 
 const Post = mongoose.model("Post", PostSchema);
